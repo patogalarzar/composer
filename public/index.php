22b734f08deddac79a2c2c7b5c6425abb8d106be
Patricio Galarza <patricio.galarza.r@gmail.com>
2016-01-26 23:22:57 -0500

    design

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<LINK REL=StyleSheet HREF="../assets/style.css" TYPE="text/css" MEDIA=screen>
+	<head>
+		<title>Certificate gnerator</title>
+	</head>
+	<body>
+		<h1>Descarga tu certificado</h1>
+		<a href="download.php" target ="_self">
+			<input type="button" name ="descargar" value ="Descargar">
+		</a>
+	</body>
+</html>
