dcb8c14138b676f1d62f1e8b8dc9901df6f72693
Patricio Galarza <patricio.galarza.r@gmail.com>
2016-01-27 14:37:40 -0500

    final landing

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html>
-	<LINK REL=StyleSheet HREF="../assets/style.css" TYPE="text/css" MEDIA=screen>
+	<LINK REL=StyleSheet HREF="assets/style.css" TYPE="text/css" MEDIA=screen>
 	<head>
 		<title>Certificate generator</title>
 	</head>
 	<body>
-		<h1>Download your certificate here</h1>
-		<a href="download.php" target ="_self">
-			<input type="button" name ="download" value ="Download" class ="boton">
-		</a>
+		<div>
+			<h1>Download your certificate here</h1>
+			<a href="download.php" target ="_self">Download</a>
+		</div>
 	</body>
 </html>
