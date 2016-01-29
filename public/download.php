1e64e9d8ca41daf34ea39c27e435c1d0491b5cd5
Patricio Galarza <patricio.galarza.r@gmail.com>
2016-01-29 16:53:07 -0500

    Autoload and namespaces

--- public/download.php
+++ public/download.php
@@ -1,14 +1,11 @@
 <?php 
 // Composer's auto-loading functionality
-// require "../vendor/autoload.php";
+require "../vendor/autoload.php";
 
 $data = array(	"name"   => "Patricio Galarza",
 				"course" => "Composer & DOMPDF"
 			);
 
-require "../certificate/template.php";
-$html =Template::render('pdf/certificate',$data);
-
-require "../certificate/pdf.php";
-Pdf::render('certificate',$html);
+$html = App\Core\Template::render('pdf/certificate',$data);
+App\Helpers\Pdf::render('certificate',$html);
 ?>
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Dompdf\\' => array($vendorDir . '/dompdf/dompdf/src'),
+    'App\\' => array($baseDir . '/Certificate'),
 );
